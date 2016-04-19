3696f63e425cb99f5728a6a086dc66254d650cad
Well <309635368@qq.com>
2016-04-19 21:30:02 +1200

    Use nginx site configurations from our repository in the development enviroment. Get rid of old alias for site files and instead update nginx configuration to use the vagrant shared www folder

--- provision.sh
+++ provision.sh
@@ -2,7 +2,7 @@ apt-get -y update
 
 apt-get -y install nginx
 
-rm -rf /usr/share/nginx/www/
-ln -s /vagrant/www /usr/share/nginx/www
+rm -rf /etc/nginx/sites-enabled
+cp -r /vagrant/sites-enabled /etc/nginx
 
 service nginx start
